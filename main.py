bda2e91a65fef8dd5bfec6556336fa836bfb7c2b
girhub11 <github11220@gmail.com>
2026-07-02 21:16:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ from aiogram import Bot, Dispatcher
 from database import init_db
 from user_handlers import user_router
 from admin_panel import admin_router
+import os
+
+# Configured details
+TOKEN = "8817446556:AAEaUREcxyfejnOhJbGabPuIip-PG8UssXE"
 
 async def main():
     await init_db()
-    bot = Bot(token="YOUR_BOT_TOKEN_HERE")
+    bot = Bot(token=TOKEN)
     dp = Dispatcher()
-    
     dp.include_router(user_router)
     dp.include_router(admin_router)
-    
+    print("Bot is running...")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
